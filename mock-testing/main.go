ebb4ed6d067845b2432206ab6f9967615454591f
sadith.nunez <sadnr0213@gmail.com>
2022-03-20 17:49:51 -0500

    TOPIC: mock tests

--- mock-testing/main.go
+++ mock-testing/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 type Person struct {
 	DNI  string
 	Name string
@@ -10,7 +12,37 @@ type Employee struct {
 	Id       int
 	Position string
 }
-type FullTimEmployee struct {
+type FullTimeEmployee struct {
 	Employee
 	Person
+}
+
+var GetPersonByDNI = func(dni string) (Person, error) {
+	time.Sleep(time.Second * 5)
+	// SELECT * FROM PERSON WHERE...
+	return Person{}, nil
+}
+var GetEmployeeById = func(id int) (Employee, error) {
+	time.Sleep(time.Second * 5)
+	// SELECT * FROM PERSON WHERE...
+	return Employee{}, nil
+}
+
+func GetFullTimeEmployeeById(id int, dni string) (FullTimeEmployee, error) {
+	var ftEmployee FullTimeEmployee
+
+	e, err := GetEmployeeById(id)
+	if err != nil {
+		return ftEmployee, err
+	}
+
+	ftEmployee.Employee = e
+
+	p, err := GetPersonByDNI(dni)
+	if err != nil {
+		return ftEmployee, err
+	}
+	ftEmployee.Person = p
+
+	return ftEmployee, nil
 }
